4953eb467238f234f8a386d3f2295dd12893c3f4
Joaquin <joaquingc123@gmail.com>
2019-01-01 16:30:25 -0500

    Add shopitem loader and update adidas spider with loader

--- ShopSpiders/ShopSpiders/spiders/adidas.py
+++ ShopSpiders/ShopSpiders/spiders/adidas.py
@@ -2,39 +2,35 @@
 import scrapy
 import re
 
+from ShopSpiders.loaders.shop_loaders import ShopItemLoader
+
 
 class AdidasSpider(scrapy.Spider):
     name = 'adidas'
     allowed_domains = ['adidas.pe']
     start_urls = ['https://m.adidas.pe/on/demandware.store/Sites-adidas-PE-Site/es_PE/Search-BrowseCatalog']
+
     def parse(self, response):
         adidas_items = response.xpath('//*[contains(@class, "innercard")]')
         for adidas_item in adidas_items:
-            item_name = adidas_item.xpath('.//*[@class="title"]/text()').extract_first()
-            item_price = re.findall(r'\d+\.\d+|\d+',adidas_item.xpath('.//*[@class="new-plp-layout-enabled"]/div/*[contains(@class, "salesprice")]/text()').extract_first())[0]
-            item_image = adidas_item.xpath('.//*[@class="show lazyload"]/@data-original').extract_first()
-            item_url = adidas_item.xpath('.//*[@class="image plp-image-bg"]/a/@href').extract_first()
+            shop_loader = ShopItemLoader(item={}, selector=adidas_item)
+            shop_loader.add_value('regular_price', re.findall(r'\d+\.\d+|\d+', adidas_item.xpath(
+                './/*[@class="new-plp-layout-enabled"]/div/*[contains(@class, "salesprice")]/text()').extract_first())[0])
+            shop_loader.add_xpath('name', './/*[@class="title"]/text()')
+            shop_loader.add_xpath('url', './/*[@class="image plp-image-bg"]/a/@href')
+            shop_loader.add_xpath('img_url', './/*[@class="image plp-image-bg"]/a/@href')
             item_width = adidas_item.xpath('.//*[@class="rating-stars rating-stars-filled"]/@style').extract_first()
- 
-            if item_width is None:
-                item_stars = 'Not Ranked'
-            else:
-                item_stars = int(re.findall(r'\d+', item_width)[0])/20
-            
-            item_stock = adidas_item.xpath('.//*[@class="badge soldout"]/*[@class="badge-text"]/text()').extract_first()
+            item_stars = str(int(re.findall(r'\d+', item_width)[0]) / 20) if item_width else 'Not Ranked'
 
-            if item_stock is None:
-                stock = 'Yes'
-            else:
-                stock = 'No'
+            shop_loader.add_value('stars', item_stars)
 
-            yield{  'Name' : item_name,
-                    'Prices' : item_price,
-                    'Image' : item_image,
-                    'URL' : item_url,
-                    'Stars' : item_stars,
-                    'Stock' : stock,
-                  }
-        next_page_url = self.start_urls[0] + response.xpath('//*[contains(@class, "next-page pagging-cta")]/@href').extract_first()
+            item_stock = 'true' if response.xpath('.//*[@class="badge soldout"]/*[@class="badge-text"]/text()').extract_first() else 'false'
+
+            shop_loader.add_value('stock', item_stock)
+
+            yield shop_loader.load_item()
+
+        next_page_url = self.start_urls[0] + response.xpath(
+            '//*[contains(@class, "next-page pagging-cta")]/@href').extract_first()
         if next_page_url:
             yield response.follow(url=next_page_url, callback=self.parse)
