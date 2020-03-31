569daf7abe37e6c16d323bd6c3a6f58493d13a98
Joaquin <joaquingc123@gmail.com>
2020-03-30 19:54:34 -0500

    Add styles to header, navbar.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+// import "bootstrap/dist/css/bootstrap.min.css"
+import "bulma/css/bulma.min.css"
+import Header from './components/Header'
+
+export default function App(): JSX.Element {
+    return (
+        <div>
+            <Header></Header>
+        </div>
+    )
+
+}
