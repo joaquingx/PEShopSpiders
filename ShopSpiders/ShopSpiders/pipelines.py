90bef70e15a9d39de1b3125029da6804e1ac8de2
Joaquin <joaquingc123@gmail.com>
2020-04-23 23:20:46 -0500

    Add space in the end of shop loaders.

--- ShopSpiders/ShopSpiders/pipelines.py
+++ ShopSpiders/ShopSpiders/pipelines.py
@@ -4,13 +4,9 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-from utils.mongo_db_utils import get_collection
 
 
 class ShopspidersPipeline(object):
-    def __init__(self):
-        self.items = list(get_collection().find({}))  # Gets mongo db in memory :scream_cat:
-        self.collection = get_collection()
 
     def process_item(self, item, spider):
         if 'currency' not in item:
