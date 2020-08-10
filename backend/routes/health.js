bb4402adc9b1e55aa7dc98fd0ffda94dce2cef28
Joaquin <joaquingc123@gmail.com>
2020-08-10 16:40:13 -0500

    Delete js backend files.

--- backend/routes/health.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-router.route('/').get((req, res) => {
-    res.send('<h1 style="color: blue; margin-left: 45%">Healthy server!<h1>')
-});
-
-module.exports = router;
--- backend/utils/routes_utils.js
+++
@@ -1,6 +0,0 @@
-export default function appendToArray(arr, toAppend){
-    if(typeof toAppend == 'array'){
-        toAppend = [toAppend]
-    }
-    return arr.concat(toAppend)
-}
